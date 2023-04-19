8cb294d53f811aa398d49ccfb78fefe78ecd102a
Dmytri Kleiner <dk@trick.ca>
2023-04-19 19:09:25 +0200

    initial

---
+++ tests/playwright/index.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+var appUrl = `http://${process.env.APP_URL}/`;
+
+test('meta is correct', async ({ page }) => {
+  await page.goto(appUrl);
+  await expect(page).toHaveTitle('Welcome to epam.');
+});
+
+test('welcome is correct', async ({ page }) => {
+  await page.goto(appUrl);
+  await expect(page.getByText('Welcome to epam.', { exact: true })).toBeVisible();
+});
